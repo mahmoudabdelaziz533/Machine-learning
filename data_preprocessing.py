3f289208d07b6f0c7dd4371790f9175f46e003d9
Mahmoud abdelaziz <60355133+mahmoudabdelaziz533@users.noreply.github.com>
2025-03-31 18:07:14 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -60,3 +60,6 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=.2,random_state=0
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
+
+*** for balancing data ***
+ we use SMOTEtomek lib 
